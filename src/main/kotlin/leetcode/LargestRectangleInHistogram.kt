f49059218baf938e13719746444050372867b528
Giorgio Abelli <giorgio.abelli99@gmail.com>
2024-10-08 19:13:10 +0200

    Improve existing solutions

--- src/main/kotlin/leetcode/LargestRectangleInHistogram.kt
+++ src/main/kotlin/leetcode/LargestRectangleInHistogram.kt
@@ -3,7 +3,29 @@ package leetcode
 import kotlin.math.max
 
 class LargestRectangleInHistogram {
+    // Based on https://youtu.be/lktr76SxB2w?si=WjON-OpfcJyH3bPU
     fun largestRectangleArea(heights: IntArray): Int {
+        val stack = mutableListOf<Int>()
+        var result = 0
+
+        fun update(right: Int) {
+            val mid = stack.removeLast()
+            val left = if (stack.isEmpty()) -1 else stack.last()
+            result = max(result, heights[mid] * (right - left - 1))
+        }
+
+        for (right in heights.indices) {
+            while (stack.isNotEmpty() && heights[stack.last()] > heights[right]) update(right)
+            stack.add(right)
+        }
+
+        while (stack.isNotEmpty()) update(heights.size)
+
+        return result
+    }
+
+    // Based on a vague memory I have from the first NeetCode 24 hours stream
+    fun largestRectangleAreaOriginal(heights: IntArray): Int {
         val stack = mutableListOf<Pair</* index */ Int, /* height */ Int>>(Pair(-1, 0))
         var result = 0
         for (i in heights.indices) {
